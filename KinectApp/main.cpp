58bdd123abd5abbf7b063e877b4e521dd6fed2ac
Kobi Jones <kobijonesmail@gmail.com>
2021-01-29 00:18:04 -0600

    Initial Commit

---
+++ KinectApp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  KinectApp
+//
+//  Created by Kobi Jones on 1/29/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
